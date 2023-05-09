3332a3a7a4b6ab631f8e959f00f12bf7556cf5d0
nbeu0760 <nbeu0760@gmail.com>
2023-05-09 13:16:27 -0400

    commmitttt

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '18ec579a20ec5fdef9d768fe6fe45ff2a3612f1a',
         'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '18ec579a20ec5fdef9d768fe6fe45ff2a3612f1a',
             'dev_requirement' => false,
         ),
         'scottjs/wp-dotenv' => array(
